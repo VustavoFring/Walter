f045b15fa631df49899a8215c20ea5ef79e28a47
VustavoFring <mver3894@gmail.com>
2023-09-16 19:56:29 +0300

    3django commit

--- Task_8_m3/advertisements/app_advertisements/models.py
+++ Task_8_m3/advertisements/app_advertisements/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+# описываем поля модели(обьявление)
+
+# заголовок
+# Описание
+# дата создания
+# дата обновления
+# Цена
+# Уместен ли торг?
+
+class Advertisement(models.Model):
+    # поле для загаловка
+    title = models.CharField('заголовок', max_length=130)
+    # описание
+    description = models.TextField('описание')
+    # цена
+    price = models.DecimalField('цена', max_digits=14, decimal_places=2)
+    # торг
+    auction = models.BooleanField('торг', help_text='отметьте если торг уместен')
+    # дата создания
+    created_at = models.DateTimeField(auto_now_add=True)
+    # дата обновления
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta: 
+        db_table = 'advertisements'
+
+    def __str__(self): 
+        return f'<Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})>'
--- Task_8_m3/advertisements/app_advertisements/views.py
+++ Task_8_m3/advertisements/app_advertisements/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # запрос на отображение глав. страницы сайта (inde.html)
-def index(reqest):
-    return HttpResponse('успешное подключение')
+def index(request):
+    return render(request, 'index.html')
+
+# запрос на отображение страници топ продавцов
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
